4b0b57f252afa132901376adbdac1275a332878a
Trecx <admin@tre.cx>
2023-07-03 12:58:54 +0200

    Code handling changes.

--- Sources/MarkdownUI/Extensibility/CodeSyntaxHighlighter.swift
+++ Sources/MarkdownUI/Extensibility/CodeSyntaxHighlighter.swift
@@ -5,26 +5,32 @@ import SwiftUI
 /// To configure the current code syntax highlighter for a view hierarchy, use the
 /// `markdownCodeSyntaxHighlighter(_:)` modifier.
 public protocol CodeSyntaxHighlighter {
-  /// Returns a text view configured with the syntax highlighted code.
-  /// - Parameters:
-  ///   - code: The code block.
-  ///   - language: The language of the code block.
-  func highlightCode(_ code: String, language: String?) -> Text
+    /// Returns a text view configured with the syntax highlighted code.
+    /// - Parameters:
+    ///   - code: The code block.
+    ///   - language: The language of the code block.
+    func codeView(_ code: String, language: String?) -> AnyView
+    func fullscreenView(_ code: String, language: String?) -> AnyView
 }
 
 /// A code syntax highlighter that returns unstyled code blocks.
 public struct PlainTextCodeSyntaxHighlighter: CodeSyntaxHighlighter {
-  /// Creates a plain text code syntax highlighter.
-  public init() {}
-
-  public func highlightCode(_ code: String, language: String?) -> Text {
-    Text(code)
-  }
+    
+    /// Creates a plain text code syntax highlighter.
+    public init() {}
+    
+    public func codeView(_ code: String, language: String?) -> AnyView {
+        AnyView(Text(code))
+    }
+    
+    public func fullscreenView(_ code: String, language: String?) -> AnyView {
+        AnyView(Text(code))
+    }
 }
 
 extension CodeSyntaxHighlighter where Self == PlainTextCodeSyntaxHighlighter {
-  /// A code syntax highlighter that returns unstyled code blocks.
-  public static var plainText: Self {
-    PlainTextCodeSyntaxHighlighter()
-  }
+    /// A code syntax highlighter that returns unstyled code blocks.
+    public static var plainText: Self {
+        PlainTextCodeSyntaxHighlighter()
+    }
 }
--- Sources/MarkdownUI/Views/Blocks/CodeBlockView.swift
+++ Sources/MarkdownUI/Views/Blocks/CodeBlockView.swift
@@ -1,30 +1,35 @@
 import SwiftUI
 
 struct CodeBlockView: View {
-  @Environment(\.theme.codeBlock) private var codeBlock
-  @Environment(\.codeSyntaxHighlighter) private var codeSyntaxHighlighter
-
-  private let fenceInfo: String?
-  private let content: String
-
-  init(fenceInfo: String?, content: String) {
-    self.fenceInfo = fenceInfo
-    self.content = content.hasSuffix("\n") ? String(content.dropLast()) : content
-  }
-
-  var body: some View {
-    self.codeBlock.makeBody(
-      configuration: .init(
-        language: self.fenceInfo,
-        content: self.content,
-        label: .init(self.label)
-      )
-    )
-  }
-
-  private var label: some View {
-    self.codeSyntaxHighlighter.highlightCode(self.content, language: self.fenceInfo)
-      .textStyleFont()
-      .textStyleForegroundColor()
-  }
+    @Environment(\.theme.codeBlock) private var codeBlock
+    @Environment(\.codeSyntaxHighlighter) private var codeSyntaxHighlighter
+    
+    @State private var showFullscreen: Bool = false
+    
+    private let fenceInfo: String?
+    private let content: String
+    
+    init(fenceInfo: String?, content: String) {
+        self.fenceInfo = fenceInfo
+        self.content = content.hasSuffix("\n") ? String(content.dropLast()) : content
+    }
+    
+    var body: some View {
+        VStack {
+            codeSyntaxHighlighter
+                .codeView(self.content, language: self.fenceInfo)
+        }
+        .sheet(isPresented: $showFullscreen) {
+            codeSyntaxHighlighter.fullscreenView(self.content, language: self.fenceInfo)
+        }
+        .onTapGesture {
+            showFullscreen = true
+        }
+    }
+    
+    private var label: some View {
+        self.codeSyntaxHighlighter.codeView(self.content, language: self.fenceInfo)
+            .textStyleFont()
+            .textStyleForegroundColor()
+    }
 }
